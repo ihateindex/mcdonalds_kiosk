bf2f96f8bd2ef003b7f1e6dc6eef9fc9be42d024
ihateindex <47144148+ihateindex@users.noreply.github.com>
2021-08-24 15:38:15 +0900

    0824 - App,js 작업중

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import GlobalStyle from './styles/GlobalStyle';
+import { BrowserRouter, Route } from 'react-router-dom';
+// import Intro from './page/Intro';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <GlobalStyle />
+            <header className="App-header">
+                <div className="nav-country">
+                    <button type="button" className="btn-change-country usa">
+                    </button>
+                </div>
+            </header>
+            <div className="App-title-block">
+                <img className="App-logo" src="./assets/mcdonalds_logo.svg" alt="" />
+                <h2 className="App-title">
+                    Where will you be
+                    <br />
+                    eating today?
+                </h2>
+            </div>
+            {/* <BrowserRouter>
+            
+            </BrowserRouter> */}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/page/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Intro.css';
+
+function Intro() {
+    return (
+        <div className="intro">
+            <h2>맥도날드</h2>
+        </div>
+    )
+}
+
+export default Intro;
