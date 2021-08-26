adf0a86e4a386e151685386645ee30e956e124e1
ihateindex <47144148+ihateindex@users.noreply.github.com>
2021-08-26 10:13:45 +0900

    0826 - Main.js 인기메뉴 추가

--- src/page/Main.js
+++ src/page/Main.js
@@ -4,9 +4,6 @@ function Main(props) {
     console.log(props);
     return (
         <div className="Main">
-            <div className="nav-country">
-                <button type="button" className="btn-change-country usa"></button>
-            </div>
             <div className="inner">
                 <div className="menu">
                     <div className="title-block">
@@ -62,28 +59,47 @@ function Main(props) {
                             <p className="price">$ 3.79</p>
                         </li>
                         <li>
-                            <img src="/assets/big_mac.jpeg" alt="" />
-                            <p className="name">Big Mac</p>
+                            <img src="/assets/quarter_pounder_cheese.jpeg" alt="" />
+                            <p className="name">Quarter Pounder</p>
                             <p className="price">$ 3.79</p>
                         </li>
                         <li>
-                            <img src="/assets/big_mac.jpeg" alt="" />
-                            <p className="name">Big Mac</p>
+                            <img src="/assets/cheeseburger.jpeg" alt="" />
+                            <p className="name">Cheeseburger</p>
+                            <p className="price">$ 2.18</p>
+                        </li>
+                        <li>
+                            <img src="/assets/deluxe_crispy_chicken_sandwich.jpeg" alt="" />
+                            <p className="name">
+                                crispy chicken
+                                <br />
+                                deluxe
+                            </p>
                             <p className="price">$ 3.79</p>
                         </li>
                         <li>
-                            <img src="/assets/big_mac.jpeg" alt="" />
-                            <p className="name">Big Mac</p>
+                            <img src="/assets/crispy_caesar_chicken_mcwrap.jpeg" alt="" />
+                            <p className="name">Crispy Chicke Wrap</p>
+                            <p className="price">$ 1.49</p>
+                        </li>
+                        <li>
+                            <img src="/assets/filet_o_fish.jpeg" alt="" />
+                            <p className="name">Filet-O-Fish</p>
                             <p className="price">$ 3.79</p>
                         </li>
                         <li>
-                            <img src="/assets/big_mac.jpeg" alt="" />
-                            <p className="name">Big Mac</p>
+                            <img src="/assets/filet_o_fish.jpeg" alt="" />
+                            <p className="name">Filet-O-Fish</p>
                             <p className="price">$ 3.79</p>
                         </li>
                         <li>
-                            <img src="/assets/big_mac.jpeg" alt="" />
-                            <p className="name">Big Mac</p>
+                            <img src="/assets/filet_o_fish.jpeg" alt="" />
+                            <p className="name">Filet-O-Fish</p>
+                            <p className="price">$ 3.79</p>
+                        </li>
+                        <li>
+                            <img src="/assets/filet_o_fish.jpeg" alt="" />
+                            <p className="name">Filet-O-Fish</p>
                             <p className="price">$ 3.79</p>
                         </li>
                     </ul>
@@ -100,6 +116,61 @@ function Main(props) {
                         Order
                     </h2>
                     <p className="desc">{props.location.state.title}</p>
+                </div>
+                <ul className="cart-list">
+                    <li>
+                        <div className="pic">
+                            <img src="/assets/big_mac.jpeg" alt="" />
+                        </div>
+                        <p className="name">Big Mac</p>
+                        <p className="price">$ 3.79</p>
+                        <div className="quantity-block">
+                            <button type="button" className="btn-minus">
+                                -
+                            </button>
+                            <div className="quantity">1</div>
+                            <button type="button" className="btn-plus">
+                                +
+                            </button>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="pic">
+                            <img src="/assets/filet_o_fish.jpeg" alt="" />
+                        </div>
+                        <p className="name">Filet-O-Fish</p>
+                        <p className="price">$ 3.79</p>
+                        <div className="quantity-block">
+                            <button type="button" className="btn-minus">
+                                -
+                            </button>
+                            <div className="quantity">1</div>
+                            <button type="button" className="btn-plus">
+                                +
+                            </button>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="pic">
+                            <img src="/assets/world_famous_fries.jpeg" alt="" />
+                        </div>
+                        <p className="name">Large Fries</p>
+                        <p className="price">$ 2.49</p>
+                        <div className="quantity-block">
+                            <button type="button" className="btn-minus">
+                                -
+                            </button>
+                            <div className="quantity">1</div>
+                            <button type="button" className="btn-plus">
+                                +
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+                <div className="total-price-block">
+                    <h5 className="title">Total</h5>
+                    <h2 className="total-price">$13.86</h2>
+                    <button type="button" className="btn-submit">Done</button>
                 </div>
             </div>
         </div>
